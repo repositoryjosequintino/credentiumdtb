b19140c33f9198d4b1b757eea2ef5dcd9c6619d1
José Quintino <josequintino@hotmail.com.br>
2025-09-24 10:35:44 -0300

    refactor: Alteração da modelagem para tenant de ususário

--- CREDENTIUM20250924094941DTB.sql
+++ CREDENTIUM20250924094941DTB.sql
@@ -14,6 +14,20 @@ drop table if exists tb_autenticacao cascade;
 drop table if exists tb_credencial cascade;
 drop table if exists tb_banco_dados cascade;
 
+create table if not exists tb_usuario (
+	code serial not null,
+	code_public uuid not null default uuid_generate_v4(),
+	nome varchar (255) not null,
+	usuario varchar (255) not null,
+	senha varchar (255) not null,
+	created_at timestamp not null default now(),
+	updated_at timestamp null,
+	deleted_at timestamp null,
+	active boolean default true,
+	constraint pk_usuario primary key (code),
+	constraint un_usuario unique (nome, usuario)
+);
+
 create table if not exists tb_categoria_pessoa (
 	code serial not null,
 	code_public uuid not null default uuid_generate_v4(),
@@ -31,6 +45,7 @@ create table if not exists tb_pessoa (
 	code serial not null,
 	code_public uuid not null default uuid_generate_v4(),
 	id_categoria_pessoa bigint not null,
+	id_usuario bigint not null,
 	nome varchar (255) not null,
 	created_at timestamp not null default now(),
 	updated_at timestamp null,
@@ -38,21 +53,8 @@ create table if not exists tb_pessoa (
 	active boolean default true,
 	constraint pk_pessoa primary key (code),
 	constraint fk_categoria_pessoa foreign key (id_categoria_pessoa) references tb_categoria_pessoa (code) on delete cascade,
+	constraint fk_usuario foreign key (id_usuario) references tb_usuario (code) on delete cascade,
 	constraint un_pessoa unique (nome)
-);
-
-create table if not exists tb_usuario (
-	code serial not null,
-	code_public uuid not null default uuid_generate_v4(),
-	id_pessoa bigint not null,
-	usuario varchar (255) not null,
-	senha varchar (255) not null,
-	created_at timestamp not null default now(),
-	updated_at timestamp null,
-	deleted_at timestamp null,
-	active boolean default true,
-	constraint pk_usuario primary key (code),
-	constraint un_usuario unique (id_pessoa, usuario)
 );
 
 create table if not exists tb_autenticacao (
@@ -75,6 +77,7 @@ create table if not exists tb_credencial (
 	id_autenticacao bigint not null,
 	usuario varchar (255) not null,
 	senha varchar (255) not null,
+	token varchar (255) null,
 	created_at timestamp not null default now(),
 	updated_at timestamp null,
 	deleted_at timestamp null,
@@ -114,18 +117,21 @@ create table if not exists tb_banco_dados (
 insert into tb_categoria_pessoa (descricao, sigla) values ('Pessoa Física', 'PF');
 insert into tb_categoria_pessoa (descricao, sigla) values ('Pessoa Jurídica', 'PJ');
 
-insert into tb_pessoa (id_categoria_pessoa, nome) values (
+insert into tb_usuario (nome, usuario, senha) values (
+	'José Quintinno',
+	'email.principal.outlook.com.br', 
+	'senha-master'
+);
+
+insert into tb_pessoa (id_categoria_pessoa, id_usuario, nome) values (
 	(select code from tb_categoria_pessoa where sigla = 'PF'),
+	(1),
 	'José Quintinno'
 );
 
-insert into tb_usuario (id_pessoa, usuario, senha) values (
-	(select code from tb_pessoa where nome = 'José Quintinno'),
-	'email.principal.outlook.com.br', 'senha-master'
-);
-
-insert into tb_pessoa (id_categoria_pessoa, nome) values (
+insert into tb_pessoa (id_categoria_pessoa, id_usuario, nome) values (
 	(select code from tb_categoria_pessoa where sigla = 'PJ'),
+	(1),
 	'Microsoft'
 );
 
@@ -150,4 +156,3 @@ insert into tb_banco_dados (id_autenticacao, hostname, databasename, portnumber,
 	1,
 	'localhost', 'db_credential', '5432', 'root', 'senha-segura'
 );
-
